babaaa50c9d02d0edaa3bc2db6983eb64335447a
Mirtyn <mirtyn@mirtyn.be>
2023-03-26 13:53:39 +0200

    version 1.0.0

--- Assets/Managers/GameManager.cs
+++ Assets/Managers/GameManager.cs
@@ -12,7 +12,7 @@ internal class GameManager
 
     public bool IsPaused = false;
 
-    public static string Version = "0.4.0";
+    public static string Version = "1.0.0";
 
     public GameType GameType = GameType.None;
 
